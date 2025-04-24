d309f10d17aede4404a00e327384df943b2021fa
lopaspi89 <lopaspi89@gmail.com>
2025-04-24 01:07:22 -0500

    Actualizacion

--- Tramada20/app/src/main/java/com/example/tramada20/app/models/LoginRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.example.tramada20.app.models
-
-data class LoginRequest(
-    val email: String,
-    val password: String
-)
--- Tramada20/app/src/main/java/com/example/tramada20/app/models/LoginResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.tramada20.app.models
-
-data class LoginResponse(
-    val id: Int,
-    val email: String,
-    val password: String,
-    val id_rol: Int
-)
-
